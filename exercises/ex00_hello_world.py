691ae2e25f1fdc01adc583bf23484990e1fa6a6c
Tadhg Sheeran <tadhg@ad.unc.edu>
2024-08-25 18:19:11 -0400

    Finished Exercise 00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,8 @@
+"""My first exercise in COMP110!"""
+
+__author__ = "730625614"
+
+
+def greet(name: str) -> str:
+    """A welcoming first function definition."""
+    return "Hello, " + name + "!"
